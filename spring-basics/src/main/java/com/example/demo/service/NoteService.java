f5b1e530e4155b634b40515de4bb423db04ef82a
Gunavaty <gunavatytraining.com>
2026-04-10 15:57:02 +0530

    api calls created

--- spring-basics/src/main/java/com/example/demo/service/NoteService.java
+++ spring-basics/src/main/java/com/example/demo/service/NoteService.java
@@ -1,21 +1,30 @@
 package com.example.demo.service;
 
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
-//import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
+import com.example.demo.repository.Order1Repository;
+import com.example.demo.entity.Order1;	
 
-import com.example.demo.entity.Order1;
-
-//@Component
 @Service
 public class NoteService {
-	public Order1 getOrder() {
-		Order1 order= new Order1();
-		order.setItem("Laptop");
-		order.setPrice(100F);
-		return order;
+	
+	@Autowired
+	Order1Repository order1Repository;
+	
+	public Iterable<Order1> getOrder() {
+		return order1Repository.findAll();
 	}
 	public Integer addOrder(Order1 order1) {
-		return 1;
+		 order1Repository.save(order1);
+		 return order1.getId();
+	}
+	public Optional<Order1> getOrderById(Integer id){
+		return order1Repository.findById(id);
+	}
+	public void deleteOrderById(Integer id){
+		 order1Repository.deleteById(id);
 	}
 }
