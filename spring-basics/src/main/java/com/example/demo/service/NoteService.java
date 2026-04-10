2f552e5947200e9400a6d723682c540d94222e7f
Gunavaty <gunavatytraining.com>
2026-04-10 16:53:10 +0530

    transactions are added

--- spring-basics/src/main/java/com/example/demo/service/NoteService.java
+++ spring-basics/src/main/java/com/example/demo/service/NoteService.java
@@ -1,10 +1,13 @@
 package com.example.demo.service;
 
+import java.io.IOException;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
 import com.example.demo.repository.Order1Repository;
 import com.example.demo.entity.Order1;	
 
@@ -13,12 +16,20 @@ public class NoteService {
 	
 	@Autowired
 	Order1Repository order1Repository;
+	@Autowired
+	PaymentService paymentService;
+	@Autowired
+	EmailService emailService;
 	
 	public Iterable<Order1> getOrder() {
 		return order1Repository.findAll();
 	}
-	public Integer addOrder(Order1 order1) {
-		 order1Repository.save(order1);
+	@Transactional(rollbackFor = Exception.class, noRollbackFor = {IOException.class})
+	public Integer addOrder(Order1 order1) throws IOException {
+		paymentService.processPayment();
+		order1Repository.save(order1);
+		emailService.send(order1.getId());
+		 
 		 return order1.getId();
 	}
 	public Optional<Order1> getOrderById(Integer id){
